99573e3809edf2246b113cd01795e478cec63924
xinlei.si <sixinlei0927@gmail.com>
2020-07-08 10:55:20 +0800

    add: 617_MergeTwoBinaryTrees

---
+++ 617.MergeTwoBinaryTrees/mergeTrees_test.go
@@ -0,0 +1,61 @@
+package _617_MergeTwoBinaryTrees
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_mergeTrees(t *testing.T) {
+	ast := assert.New(t)
+
+	tests := []struct {
+		tree1 *TreeNode
+		tree2 *TreeNode
+		want  *TreeNode
+	}{
+		{
+			tree1: &TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val:   3,
+					Left:  &TreeNode{Val: 5, Left: nil, Right: nil},
+					Right: nil},
+				Right: &TreeNode{
+					Val:   2,
+					Left:  nil,
+					Right: nil}},
+
+			tree2: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   1,
+					Left:  nil,
+					Right: &TreeNode{Val: 4, Left: nil, Right: nil}},
+				Right: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: &TreeNode{Val: 7, Left: nil, Right: nil}}},
+			want: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val:   4,
+					Left:  &TreeNode{Val: 5, Left: nil, Right: nil},
+					Right: &TreeNode{Val: 4, Left: nil, Right: nil},
+				},
+				Right: &TreeNode{
+					Val:  5,
+					Left: nil,
+					Right: &TreeNode{
+						Val:   7,
+						Left:  nil,
+						Right: nil,
+					},
+				},
+			},
+		},
+	}
+
+	for _, item := range tests {
+		ast.Equal(item.want, mergeTrees(item.tree1, item.tree2), "")
+	}
+}
