8455b2695490a6e9123b943e8d3750b452aedc6d
xinlei.si <sixinlei0927@gmail.com>
2020-07-09 11:05:57 +0800

    add: 102 BinaryTreeLevelOrderTraversal

---
+++ 102.BinaryTreeLevelOrderTraversal/levelOrder_test.go
@@ -0,0 +1,32 @@
+package _02_BinaryTreeLevelOrderTraversal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_levelOrder(t *testing.T) {
+	ast := assert.New(t)
+
+	tests := []struct {
+		args *TreeNode
+		want [][]int
+	}{
+		{
+			args: &TreeNode{
+				Val:  3,
+				Left: &TreeNode{Val: 9, Left: nil, Right: nil},
+				Right: &TreeNode{
+					Val:   20,
+					Left:  &TreeNode{Val: 15, Left: nil, Right: nil},
+					Right: &TreeNode{Val: 7, Left: nil, Right: nil},
+				}},
+			want: [][]int{{3}, {9, 20}, {15, 7}},
+		},
+	}
+
+	for _, item := range tests {
+		ast.Equal(item.want, levelOrder(item.args), "输入:%s", item.args)
+
+	}
+}
