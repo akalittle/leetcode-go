9ee1e7ac42b9fc556de5aba72e246ef56a22a6f6
xinlei.si <sixinlei0927@gmail.com>
2020-10-10 15:55:32 +0800

    update: 39

--- Array/39.CombinationSum.go
+++ Array/39.CombinationSum.go
@@ -52,15 +52,22 @@ func combinationSum(candidates []int, target int) [][]int {
 			return
 		}
 		if target == 0 {
-			ans = append(ans, append([]int(nil), comb...))
+			tmp := make([]int, len(comb))
+			copy(tmp, comb)
+			ans = append(ans, tmp)
 			return
 		}
-		// 直接跳过
+
+		// 具体流程可以参考
+		//https://assets.leetcode-cn.com/solution-static/39/39_fig1.png
+
+		// 不用这个数
 		dfs(target, idx+1)
 		// 选择当前数
 		if target-candidates[idx] >= 0 {
 			comb = append(comb, candidates[idx])
 			dfs(target-candidates[idx], idx)
+			// 回溯框架，进行下一步之前加入当前节点，下一步递归过之后，要把当前节点从路径中删除，所以才是“回溯”
 			comb = comb[:len(comb)-1]
 		}
 	}
